31518bc8b622bc86ddc84a294691ba5c61be1b35
strixPanahu <schimpfzach@gmail.com>
2022-11-18 20:16:53 -0600

    Insert base code

--- account.py
+++ account.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def __init__(self, name):
+    __account_name__ = name
+    __account_balance__ = 0
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def deposit(self, amount):
+    requested_balance = self.__account_balance__ + amount
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if requested_balance > 0 and amount > 0:
+        self.__account_balance__ = requested_balance
+        return True
+    else:
+        return False
+
+
+def withdraw(self, amount):
+    requested_balance = self.__account_balance__ - amount
+
+    if requested_balance > 0 and amount > 0:
+        self.__account_balance__ = requested_balance
+        return True
+    else:
+        return False
+
+
+def get_balance(self):
+    return self.__account_balance__
+
+
+def get_name(self):
+    return self.__account_name__
